45fdddbc82cc307a4a9db51def42b22e452df85b
Aushomapto Prodip <prodip_sarker@diu.edu.bd>
2016-06-06 13:49:12 +0600

    C#

---
+++ Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace CRUD_APPS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDept = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCGPA = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnView = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblMessage = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Id";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(87, 31);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(143, 25);
+            this.txtId.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(87, 71);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(143, 25);
+            this.txtName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name";
+            // 
+            // txtDept
+            // 
+            this.txtDept.Location = new System.Drawing.Point(87, 120);
+            this.txtDept.Name = "txtDept";
+            this.txtDept.Size = new System.Drawing.Size(143, 25);
+            this.txtDept.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Dept";
+            // 
+            // txtCGPA
+            // 
+            this.txtCGPA.Location = new System.Drawing.Point(87, 170);
+            this.txtCGPA.Name = "txtCGPA";
+            this.txtCGPA.Size = new System.Drawing.Size(143, 25);
+            this.txtCGPA.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "CGPA";
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Font = new System.Drawing.Font("Segoe UI Symbol", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsert.Location = new System.Drawing.Point(27, 261);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(79, 35);
+            this.btnInsert.TabIndex = 8;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Segoe UI Symbol", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(158, 261);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(85, 35);
+            this.btnUpdate.TabIndex = 9;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Segoe UI Symbol", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(291, 261);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(88, 35);
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnView
+            // 
+            this.btnView.Font = new System.Drawing.Font("Segoe UI Symbol", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnView.Location = new System.Drawing.Point(417, 261);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(79, 35);
+            this.btnView.TabIndex = 11;
+            this.btnView.Text = "View";
+            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.Click += new System.EventHandler(this.btnView_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(245, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(464, 188);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(255, 323);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(23, 17);
+            this.lblMessage.TabIndex = 13;
+            this.lblMessage.Text = "---";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(721, 380);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnView);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.txtCGPA);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtDept);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI Symbol", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CRUD";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDept;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCGPA;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblMessage;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace CRUD_APPS
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection("Data Source=PRODIP-PC;Initial Catalog=CrudDb;Integrated Security=True;Pooling=False");
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter da;
+        DataTable dt;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmd.Connection = con;
+        }
+
+        #region Insert Operation
+        
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text != "" & txtDept.Text != "" & txtCGPA.Text != "")
+            {
+                con.Open();
+                cmd = new SqlCommand("INSERT INTO info(Name,Dept,CGPA) VALUES('" + txtName.Text + "','" + txtDept.Text + "','" + txtCGPA.Text + "')", con);
+
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+                lblMessage.ForeColor = Color.Green;
+                lblMessage.Text = "Data Inserted.";
+            }
+            else
+            {
+                lblMessage.ForeColor = Color.Red;
+                lblMessage.Text = "Please Fill Tha Name,Dept and CGPA colume.";
+            }
+
+        }
+        #endregion 
+
+        #region Update Operation
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+           if(txtId.Text!="")
+           {
+            con.Open();
+            cmd = new SqlCommand("UPDATE info SET Name=@a1,Dept=@a2,CGPA=@a3 WHERE Id=@a4", con);
+
+            cmd.Parameters.Add("a1", txtName.Text);
+            cmd.Parameters.Add("a2", txtDept.Text);
+            cmd.Parameters.Add("a3", txtCGPA.Text);
+            cmd.Parameters.Add("a4", txtId.Text);
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            lblMessage.ForeColor = Color.Green;
+            lblMessage.Text = "Update Sucessfully";
+           }
+            else{
+               lblMessage.ForeColor=Color.Red;
+               lblMessage.Text = "Please fill Id colume.";
+           }
+
+        }
+
+
+        #endregion
+
+        #region Delete Operation
+    
+        
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (txtId.Text != "")
+            {
+                con.Open();
+                cmd = new SqlCommand("delete from info where Id='" + this.txtId.Text + "'", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                lblMessage.ForeColor = Color.Green;
+                lblMessage.Text = "Data Can be Deleted From DataBase.";
+            }
+            else
+            {
+                lblMessage.ForeColor = Color.Red;
+                lblMessage.Text = "Please Fill the Id Colume whose are deleted from database.";
+            }
+        }
+
+
+
+        #endregion
+
+        #region View DataBase
+        
+
+        private void btnView_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlDataAdapter da = new SqlDataAdapter("select * from info", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+
+        #endregion
+
+    }
+}
